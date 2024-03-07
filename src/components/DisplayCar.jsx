9729d0dd967e00d2a9b97ea5df1f5e864159fa2d
Glody Mafo <glodymafokis@gmail.com>
2024-03-07 23:29:10 +0200

    actualisation

--- src/components/DisplayCar.jsx
+++ src/components/DisplayCar.jsx
@@ -10,7 +10,7 @@ function DisplayCar() {
         <h2 className="text-white text-3xl font-semibold">PIXIUM 2024</h2>
         <p className="text-white w-3/12">
           Les dimensions de la Porsche 911 Carrera et Targa sont de 4 519 mm en
-          longueur, 1 852 mm en largeur, 1 298 mm en hauteur et de 2 450 mm en
+          longueur, 1 852 mm en laeur, 1 298 mm en hauteur et de 2 450 mm en
           empattement. Le poids total est de 1 505 à 1 580 kg à vide avec une
           charge autorisée à hauteur de 455 kg.
         </p>
